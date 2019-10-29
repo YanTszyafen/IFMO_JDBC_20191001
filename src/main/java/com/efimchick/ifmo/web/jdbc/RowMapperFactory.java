3efd85986adab60d7d0beb84fdba9c4113fb4cc6
Ян Цзяфэн <41192330+YanTszyafen@users.noreply.github.com>
2019-10-29 17:10:24 +0300

    02

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,40 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        ConnectionSource connectionSource = ConnectionSource.instance();
+        RowMapper<Employee> rowMapper = resultSet -> {
+            try(Connection conn = connectionSource.createConnection();
+                Statement statement = conn.createStatement();
+                ResultSet rs = statement.executeQuery("select * from EMPLOYEE")){
+                rs.next();
+                BigInteger id = new BigInteger(String.valueOf(resultSet.getInt("id")));
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String middleName = resultSet.getString("middleName");
+                FullName fullName = new FullName(firstName, lastName, middleName);
+                Position position = Position.valueOf(resultSet.getString("position"));
+                LocalDate hired = LocalDate.parse(String.valueOf(resultSet.getDate("hiredate")));
+                BigDecimal salary = new BigDecimal(String.valueOf(resultSet.getBigDecimal("salary")));
+                Employee employee = new Employee(id, fullName, position, hired, salary);
+                return employee;
+            }catch (SQLException e){
+                return null;
+            }
+        };
+        return rowMapper;
     }
 }
